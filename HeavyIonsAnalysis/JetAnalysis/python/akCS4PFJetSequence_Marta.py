e7280d5c29e41daacffb44a10e6cd0c2844ee333
Marta <marta.verweij@cern.ch>
2016-02-03 02:10:36 +0100

    python configs for foresting CS jets

---
+++ HeavyIonsAnalysis/JetAnalysis/python/akCS4PFJetSequence_Marta.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+#from RecoJets.JetProducers.ak4PFJets_cfi import ak4PFJets
+from RecoJets.JetProducers.ak8PFJetsCS_cfi import ak8PFJetsCS#, ak8PFJetsCSConstituents
+
+#change input tag
+pfInput = cms.InputTag('particleFlowTmp')
+
+#constituent subtraction
+akCs4PFJets = ak8PFJetsCS.clone( 
+    src    = pfInput,
+    rParam = cms.double(0.4),
+    jetPtMin = cms.double(0.0),
+    doAreaFastjet = cms.bool(True),
+    useConstituentSubtractionHi = cms.bool(True),
+    etaMap    = cms.InputTag('hiFJRhoProducer','mapEtaEdges'),
+    rho       = cms.InputTag('hiFJRhoProducer','mapToRho'),
+    rhom      = cms.InputTag('hiFJRhoProducer','mapToRhoM'),
+    csAlpha   = cms.double(1.)
+    )
